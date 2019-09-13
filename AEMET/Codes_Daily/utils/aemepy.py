e1c1e8e9697b74245e6af02f2d5d51b0d03a87b6
Tomas Sanchez <tssanchezpastor@gmail.com>
2019-09-13 11:48:02 +0200

    utils module

---
+++ AEMET/Codes_Daily/utils/aemepy.py
@@ -0,0 +1,35 @@
+def DF_subplots2x2(df_list, xcol, ycol, titles, ylabel='', xlabel='', c='purple', save=False, v=''):
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+    for i in range(len(df_list)):
+        ax = plt.subplot(2, 2, i+1)
+        df_list[i].plot(ax=ax, x=xcol, y=ycol, style='-', color=c)
+        plt.title('{}'.format(titles[i]), fontsize=16)
+        plt.grid(True)
+        if i == 0 or i==2:
+            plt.ylabel(r'${}$'.format(ylabel), fontsize=16)
+            plt.xlabel('')
+        elif i == 1 or i == 3:
+            plt.xlabel(r'${}$'.format(xlabel), fontsize=16)
+        else:
+            plt.xlabel('')
+        ax.get_legend().remove()
+        plt.xticks(rotation=30, fontsize=12)
+        plt.tight_layout()
+        if save:
+            plt.savefig('../Figures/{}.png'.format('_'.join(titles) + '-' + (str(v))), bbox_inches='tight')
+
+def concat_dictionary(DIC):
+        keys_list = [key for key in DIC.keys()]
+        for j in range(len(keys_list)):
+            if j == 0:
+                Final = DIC[keys_list[0]]
+            else:
+                Final = pd.concat([Final, DIC[keys_list[j]]])
+        return Final
+
+def filter_duplicates(DF):
+        duplicated = DF.duplicated()
+        filter_duplicates = DF.duplicated(keep='first')
+        return Data[~filter_duplicates]
