352131fdb71f837dc3a9eef5835d5b4c7758cdd4
Tomas Sanchez <tssanchezpastor@gmail.com>
2019-09-16 16:53:00 +0200

    corrections and first attempt to fill nulls with average per month values

--- AEMET/Codes_Daily/split_by_city.py
+++ AEMET/Codes_Daily/split_by_city.py
@@ -12,21 +12,21 @@ ZGZ = DF[DF['indicativo'] == '9434']
 HSC = DF[DF['indicativo'] == '9898']
 
 # with na
-BCN.to_csv('../Data/Daily/BCN/' + 'BCN.csv')
-NVR.to_csv('../Data/Daily/NVR/' + 'NVR.csv')
-HSC.to_csv('../Data/Daily/HSC/' + 'HSC.csv')
-ZGZ.to_csv('../Data/Daily/ZGZ/' + 'ZGZ.csv')
+BCN[(BCN['fecha'] >= '2013-07-07') & (BCN['fecha'] <= '2019-07-21')].sort_values(['fecha']).to_csv('../Data/Daily/BCN/' + 'BCN.csv')
+NVR[(NVR['fecha'] >= '2013-07-07') & (NVR['fecha'] <= '2019-07-21')].sort_values(['fecha']).to_csv('../Data/Daily/NVR/' + 'NVR.csv')
+HSC[(HSC['fecha'] >= '2013-07-07') & (HSC['fecha'] <= '2019-07-21')].sort_values(['fecha']).to_csv('../Data/Daily/HSC/' + 'HSC.csv')
+ZGZ[(ZGZ['fecha'] >= '2013-07-07') & (ZGZ['fecha'] <= '2019-07-21')].sort_values(['fecha']).to_csv('../Data/Daily/ZGZ/' + 'ZGZ.csv')
 
 # dropna
-BCN_dropna = BCN[(BCN['fecha'] > '2013-07-07') & (BCN['fecha'] < '2019-07-21')].sort_values(['fecha']).dropna()
+BCN_dropna = BCN[(BCN['fecha'] >= '2013-07-07') & (BCN['fecha'] <= '2019-07-21')].sort_values(['fecha']).dropna()
 BCN_dropna.to_csv('../Data/Daily/BCN/' + 'BCN_notnulls.csv')
 
-NVR_dropna = NVR[(NVR['fecha'] > '2013-07-07') & (NVR['fecha'] < '2019-07-21')].sort_values(['fecha']).dropna()
+NVR_dropna = NVR[(NVR['fecha'] >= '2013-07-07') & (NVR['fecha'] <= '2019-07-21')].sort_values(['fecha']).dropna()
 NVR_dropna.to_csv('../Data/Daily/NVR/' + 'NVR_notnulls.csv')
 
-HSC_dropna = HSC[(HSC['fecha'] > '2013-07-07') & (HSC['fecha'] < '2019-07-21')].sort_values(['fecha']).dropna()
+HSC_dropna = HSC[(HSC['fecha'] >= '2013-07-07') & (HSC['fecha'] <= '2019-07-21')].sort_values(['fecha']).dropna()
 HSC_dropna.to_csv('../Data/Daily/HSC/' + 'HSC_notnulls.csv')
 
-ZGZ_dropna = ZGZ[(ZGZ['fecha'] > '2013-07-07') & (ZGZ['fecha'] < '2019-07-21')].sort_values(['fecha']).dropna()
+ZGZ_dropna = ZGZ[(ZGZ['fecha'] >= '2013-07-07') & (ZGZ['fecha'] <= '2019-07-21')].sort_values(['fecha']).dropna()
 ZGZ_dropna.to_csv('../Data/Daily/ZGZ/' + 'ZGZ_notnulls.csv')
 print('Done')
