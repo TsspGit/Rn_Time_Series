1dfd6470a85d46833806e32152d4ad5866e966cd
Tomas Sanchez <tssanchezpastor@gmail.com>
2019-10-02 09:56:25 +0200

    exported from cuda

--- AEMET/Codes_Daily/CNN_RnT.py
+++ AEMET/Codes_Daily/CNN_RnT.py
@@ -42,7 +42,7 @@ ZGZ_scaled = weekly_scaled['ZGZ']
 DFscaled_list = [BCN_scaled, PMP_scaled, ZGZ_scaled, HSC_scaled]
 
 # Only Rn
-sample_size = 4
+sample_size = 3
 neuron = [64, 32]
 test_size=int(0.3 * len(mdnRnA))
 X = np.atleast_3d(np.array([mdnRnA[start:start + sample_size] for start in range(0, mdnRnA.shape[0]-sample_size)]))
@@ -52,7 +52,7 @@ Ytrain, Ytest = Y[:-test_size], Y[-test_size:]
 print("X_train.shape = ", Xtrain.shape, "\nY_train.shape = ", Ytrain.shape)
 
 ## Predict
-history, pred, acc_train, acc_test = NN(neuron, nep=35, X_train=Xtrain, Y_train=Ytrain, X_test=Xtest, Y_test=Ytest, sample_size=sample_size)
+history, pred, acc_train, acc_test = NN(neuron, nep=30, X_train=Xtrain, Y_train=Ytrain, X_test=Xtest, Y_test=Ytest, sample_size=sample_size)
 
 ## Errors
 testScoreECM = mean_squared_error(Ytest, pred)
@@ -61,20 +61,19 @@ testScoreEAM = mean_absolute_error(Ytest, pred)
 print('EAM: %.4f' % (testScoreEAM))
 
 ## Plot
-fig,ax = plt.subplots(nrows=1,ncols=1,figsize=(8,6))
-plt.figure(1)
+fig,ax = plt.subplots(nrows=1,ncols=1,figsize=(10,6))
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
 plt.ylabel('loss', fontsize=14)
 plt.xlabel('epoch', fontsize=14)
 plt.legend(['train', 'test'], loc='upper right')
 plt.tight_layout()
-plt.savefig('./CNN_Loss_Rn_{}_{}.eps'.format(neuron[0], neuron[1]))
+#plt.savefig('./CNN_Loss_Rn_{}_{}.png'.format(neuron[0], neuron[1]))
+np.savetxt('CNN_Loss_Rn_{}_{}_v2.txt'.format(neuron[0], neuron[1]), (history.history['loss'], history.history['val_loss']), delimiter=',')
 
 ## Plot2
 startdaypred = pd.datetime(2013, 7, 1) + 7*pd.Timedelta( len(mdnRnA)-len(pred), unit='D')
-fig,ax = plt.subplots(nrows=1,ncols=1,figsize=(14,4))
-plt.figure(2)
+fig,ax = plt.subplots(nrows=1,ncols=1,figsize=(8,4), constrained_layout=True)
 xaxis = ax.get_xaxis()
 ax.xaxis.set_major_locator(mdates.YearLocator())
 ax.xaxis.set_minor_locator(mdates.MonthLocator())
@@ -83,11 +82,12 @@ ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
 ax.plot(pd.date_range(startday, periods=len(mdnRnA), freq='W'), mdnRnA, 'k', alpha=0.7) 
 ax.plot(pd.date_range(startdaypred, periods=len(pred), freq='W'), pred, linewidth=2, linestyle='-',color='crimson')
 plt.xlabel('Dates', fontsize=16)
+plt.ylabel(r'$^{222}Rn\ (Bq/m^3)$', fontsize=16)
 ax.legend(['Data', 'CNN'], loc='upper left')
 plt.ylim([30, 140])
 
 from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes, mark_inset
-axins = zoomed_inset_axes(ax, 1.7, loc='lower left', bbox_to_anchor=(640,140))
+axins = zoomed_inset_axes(ax, 1.7, loc='lower left', bbox_to_anchor=(2361,700))
 axins.xaxis.set_major_locator(mdates.YearLocator())
 axins.xaxis.set_minor_locator(mdates.MonthLocator())
 axins.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
@@ -99,10 +99,8 @@ plt.xticks(visible=True)
 plt.yticks(visible=False)
 mark_inset(ax, axins, loc1=2, loc2=4, fc="none", ec="0.5")
 plt.suptitle('Weekly Fitting at LSC - Hall A')
-plt.tight_layout()
-plt.savefig('./CNN_Rn_{}_{}.eps'.format(neuron[0], neuron[1]))
-
-
+#fig.savefig('./CNN_Rn_{}_{}.png'.format(neuron[0], neuron[1]), bbox_inches='tight', dpi=300)
+np.savetxt('CNN_Rn_{}_{}_v2.txt'.format(neuron[0], neuron[1]), pred, delimiter=',', fmt='%s')
 
 
 #Rn + Temperature
@@ -116,7 +114,7 @@ Xtrain_PMP, Xtest_PMP = train_test_split(Xt_PMP, test_size)
 Xtrain_HSC, Xtest_HSC = train_test_split(Xt_HSC, test_size)
 Xtrain_ZGZ, Xtest_ZGZ = train_test_split(Xt_ZGZ, test_size)
 ## Predict
-history, pred, acc_train, acc_test = NN(neuron, nep=35, X_train=Xtrain_PMP, Y_train=Ytrain, X_test=Xtest_PMP, Y_test=Ytest, sample_size=sample_size)
+history, pred, acc_train, acc_test = NN(neuron, nep=30, X_train=Xtrain_PMP, Y_train=Ytrain, X_test=Xtest_PMP, Y_test=Ytest, sample_size=sample_size)
 
 ## Errors
 testScoreECM = mean_squared_error(Ytest, pred)
@@ -126,19 +124,18 @@ print('EAM: %.4f' % (testScoreEAM))
 
 ## Plot
 fig,ax = plt.subplots(nrows=1,ncols=1,figsize=(8,6))
-plt.figure(3)
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
 plt.ylabel('loss', fontsize=14)
 plt.xlabel('epoch', fontsize=14)
 plt.legend(['train', 'test'], loc='upper right')
 plt.tight_layout()
-plt.savefig('./CNN_Loss_RnT_PMP_{}_{}.eps'.format(neuron[0], neuron[1]))
+#plt.savefig('./CNN_Loss_RnT_PMP_{}_{}.png'.format(neuron[0], neuron[1]))
+np.savetxt('CNN_Loss_RnT_PMP_{}_{}_v2.txt'.format(neuron[0], neuron[1]), (history.history['loss'], history.history['val_loss']), delimiter=',')
 
 ## Plot2
 startdaypred = pd.datetime(2013, 7, 1) + 7*pd.Timedelta( len(mdnRnA)-len(pred), unit='D')
 fig,ax = plt.subplots(nrows=1,ncols=1,figsize=(10,4))
-plt.figure(4)
 xaxis = ax.get_xaxis()
 ax.xaxis.set_major_locator(mdates.YearLocator())
 ax.xaxis.set_minor_locator(mdates.MonthLocator())
@@ -147,11 +144,12 @@ ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
 ax.plot(pd.date_range(startday, periods=len(mdnRnA), freq='W'), mdnRnA, 'k', alpha=0.7) 
 ax.plot(pd.date_range(startdaypred, periods=len(pred), freq='W'), pred, linewidth=2, linestyle='-',color='crimson')
 plt.xlabel('Dates', fontsize=16)
-ax.legend(['Data', 'CNN'], loc='upper left')
+plt.ylabel(r'$^{222}Rn\ (Bq/m^3)$', fontsize=16)
+ax.legend(['Data', 'CNN(Rn + T)'], loc='upper left')
 plt.ylim([30, 140])
 
 from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes, mark_inset
-axins = zoomed_inset_axes(ax, 1.7, loc='lower left', bbox_to_anchor=(640,140))
+axins = zoomed_inset_axes(ax, 1.7, loc='lower left', bbox_to_anchor=(2361,700))
 axins.xaxis.set_major_locator(mdates.YearLocator())
 axins.xaxis.set_minor_locator(mdates.MonthLocator())
 axins.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
@@ -163,11 +161,8 @@ plt.xticks(visible=True)
 plt.yticks(visible=False)
 mark_inset(ax, axins, loc1=2, loc2=4, fc="none", ec="0.5")
 plt.suptitle('Weekly Fitting at LSC - Hall A')
-plt.tight_layout()
-plt.savefig('./CNN_RnT_PMP_{}_{}.eps'.format(neuron[0], neuron[1]))
-
-
-#plt.show()
+#fig.savefig('./CNN_RnT_PMP_{}_{}.png'.format(neuron[0], neuron[1]), bbox_inches='tight', dpi=300)
+np.savetxt('CNN_RnT_PMP_{}_{}_v2.txt'.format(neuron[0], neuron[1]), pred, delimiter=',', fmt='%s')
 
 
 
