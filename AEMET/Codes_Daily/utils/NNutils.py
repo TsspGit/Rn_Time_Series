91512712ed4a4187d2170d1451ce8881c3775b1c
Tomas Sanchez <tssanchezpastor@gmail.com>
2019-10-24 15:12:10 +0200

    New utilities

--- AEMET/Codes_Daily/utils/NNutils.py
+++ AEMET/Codes_Daily/utils/NNutils.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization
 from keras.layers.convolutional import Conv1D, MaxPooling1D
+from keras.models import model_from_json
 import pandas as pd  
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error, mean_absolute_error
@@ -47,14 +48,20 @@ def data_toCNN_format(DF_list, arr_str, fields, sample_size):
     output = {}
     for i in range(len(DF_list)):
         weekly_to3d = DF_list[i][fields].values
-        output[arr_str[i]] = np.array([weekly_to3d[start:start+sample_size] for start in range(0, weekly_to3d.shape[0]-sample_size)])
+        output[arr_str[i]] = np.array([weekly_to3d[start:start+sample_size] for start in range(0, weekly_to3d.shape[0]-sample_size+1)])
     return output
 
 def train_test_split(X, test_size):
     X_train, X_test = X[:-test_size], X[-test_size:]
     return X_train, X_test
 
-def NN(neurons, nep, X_train, Y_train, X_test, Y_test, sample_size, v=0, btch_size=10):
+
+def save_NN(model):
+    model.save("modelaemet.h5")
+    print("Save model to disk")
+    
+    
+def NN(neurons, nep, X_train, Y_train, X_test, Y_test, sample_size, v=0, btch_size=10, save=False):
     model = Sequential()
     model.add(Conv1D(filters=neurons[0], kernel_size=3, activation='relu', input_shape=X_train.shape[1:]))
     model.add(Flatten())
@@ -67,7 +74,145 @@ def NN(neurons, nep, X_train, Y_train, X_test, Y_test, sample_size, v=0, btch_si
     acc_train = np.average(history.history["acc"])
     acc_test = np.average(history.history["val_acc"])
     #print("Train Accuracy: ", acc_train, "\nTest Accuracy:  ", acc_test)
+    if save:
+        save_NN(model)
     return history, pred, acc_train, acc_test
+
+def NN_v2(neurons, nep, X_train, Y_train, X_test, Y_test, sample_size, v=0, btch_size=10, save=False):
+    model = Sequential()
+    model.add(Conv1D(filters=neurons[0], kernel_size=3, activation='relu', input_shape=X_train.shape[1:]))
+    model.add(Conv1D(filters=neurons[1], kernel_size=3, activation='relu'))
+    model.add(Flatten())
+    model.add(Dropout(0.4))
+    model.add(Dense(int(neurons[0]/2), activation='linear'))
+    model.add(Dense(int(neurons[1]/2), activation='linear'))
+    model.add(Dense(Y_train.shape[1], activation='linear'))
+    model.compile(loss="mae", optimizer="adam", metrics=["acc"])
+    history = model.fit(X_train, Y_train, epochs=nep, batch_size=btch_size, verbose=v, validation_data=(X_test, Y_test))
+    pred = model.predict(X_test)
+    acc_train = np.average(history.history["acc"])
+    acc_test = np.average(history.history["val_acc"])
+    #print("Train Accuracy: ", acc_train, "\nTest Accuracy:  ", acc_test)
+    if save:
+        save_NN(model)
+    return history, pred, acc_train, acc_test, model
+
+def extract_maxs_mins_avgs(M):
+    diagM = [M[::-1].diagonal(i) for i in range(-M.shape[0]+1, M.shape[1])]
+    mins = []
+    maxs = []
+    avgs = []
+    for row in range(len(diagM)):
+        mins.append(np.min(diagM[row]))
+        maxs.append(np.max(diagM[row]))
+        avgs.append(np.mean(diagM[row]))
+    return maxs, mins, avgs
+
+def plot_forecast(data, startday, pred, startdaypred):
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    fig,ax = plt.subplots(nrows=1,ncols=1,figsize=(10,4))
+    plt.figure(1)
+    xaxis = ax.get_xaxis()
+    ax.xaxis.set_major_locator(mdates.YearLocator())
+    ax.xaxis.set_minor_locator(mdates.MonthLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
+    ax.plot(pd.date_range(startday, periods=len(data), freq='W')[:-5], data[:-5], 'k') 
+    ax.plot(pd.date_range(startdaypred, periods=len(pred), freq='W')[:-5], pred[:-5],
+            linewidth=2, linestyle='-',color='crimson')
+
+    plt.xlabel('Dates', fontsize=16)
+    ax.legend(['Data', 'CNN (Rn + T)'], loc='upper left')
+    plt.ylim([30, 140])
+
+    from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes, mark_inset
+    axins = zoomed_inset_axes(ax, 1.7, loc='lower left', bbox_to_anchor=(643,140))
+    axins.xaxis.set_major_locator(mdates.YearLocator())
+    axins.xaxis.set_minor_locator(mdates.MonthLocator())
+    axins.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
+    axins.plot(pd.date_range(startday, periods=len(data), freq='W')[:-5], data[:-5],
+               'k') 
+    axins.plot(pd.date_range(startdaypred, periods=len(pred), freq='W')[:-5], pred[:-5], linewidth=2,
+               linestyle='-',color='crimson')
+    axins.set_xlim('2017-08-05', '2019-06-24')
+    axins.set_ylim(50, 120)
+    axins.set_yticks([60, 80, 100])
+    mark_inset(ax, axins, loc1=2, loc2=4, fc="none", ec="0.5")
+    
+    plt.suptitle('Weekly Fitting at LSC - Hall A')
+    plt.show()
+    
+def plot_validation(data_val, pred_val, startday_ahead):
+    import matplotlib.pyplot as plt
+    import pandas as pd
+    # ahead plot
+    fig,ax = plt.subplots(nrows=1,ncols=1,figsize=(10,4))#
+    #plt.figure(2)
+    xaxis = ax.get_xaxis()
+    ax.xaxis.set_major_locator(mdates.YearLocator())
+    ax.xaxis.set_minor_locator(mdates.MonthLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
+    ax.xaxis.set_minor_formatter(mdates.DateFormatter("%b"))
+
+    ax.plot(pd.date_range(startday_ahead, periods=len(data_val), freq='W'), data_val, color='k', linewidth=1)
+    ax.plot(pd.date_range(startday_ahead, periods=len(pred_val), freq='W'), pred_val, linestyle='-', color='g', linewidth=1)
+    plt.xlabel('Dates')
+    plt.ylim([30, 140])
+    ax.legend(['Data', 'Validation'], loc='upper left')
+    plt.show()
+    
+
+def plot_fill_errors(data, predmins, predmaxs, predavgs, errors, startday, startdaypred):
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    fig,ax = plt.subplots(nrows=1,ncols=1,figsize=(10,4))
+    plt.figure(1)
+    xaxis = ax.get_xaxis()
+    ax.xaxis.set_major_locator(mdates.YearLocator())
+    ax.xaxis.set_minor_locator(mdates.MonthLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
+    ax.fill_between(pd.date_range(startdaypred, periods=len(predmins), freq='W')[:-5], predmins[:-5], predmaxs[:-5],
+                    facecolor='g')
+    ax.plot(pd.date_range(startday, periods=len(data), freq='W')[:-5], data[:-5], 'k') 
+    ax.set_xlabel('Dates')
+    ax.set_ylabel(r'$^{222}$Rn ($Bq\cdot m^{-3}$)', fontsize=16)
+    ax.legend(['Data', 'CNN (Rn + T)'], loc='upper left')
+    ax.set_ylim([30, 140])
+    ax.grid()
+
+    from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes, mark_inset
+    axins = zoomed_inset_axes(ax, 1.7, loc='lower left', bbox_to_anchor=(673,223))
+    axins.xaxis.set_major_locator(mdates.YearLocator())
+    axins.xaxis.set_minor_locator(mdates.MonthLocator())
+    axins.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
+    axins.fill_between(pd.date_range(startdaypred, periods=len(predmins), freq='W')[:-5], predmins[:-5], predmaxs[:-5],
+                       facecolor='g')
+    axins.plot(pd.date_range(startday, periods=len(data), freq='W')[:-5], data[:-5], 'k') 
+    axins.set_xlim('2017-08-15', '2019-06-24')
+    axins.set_ylim(50, 110)
+    axins.set_yticks([60, 80, 100])
+    axins.set_title('a)', loc='right', x=0.98, y=0.85)
+    axins.grid()
+
+    axins2 = zoomed_inset_axes(ax, 1.7, loc='lower left', bbox_to_anchor=(673,97))
+    axins2.xaxis.set_major_locator(mdates.YearLocator())
+    axins2.xaxis.set_minor_locator(mdates.MonthLocator())
+    axins2.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
+    axins2.vlines(pd.date_range(startdaypred, periods=len(predavgs), freq='W')[:-5], 0, errors)
+    axins2.set_xlim('2017-08-15', '2019-06-24')
+    axins2.set_yticks([int(np.min(errors)+10), 0, int(np.max(errors)+1)])
+    axins2.set_ylim([-20, 20])
+    axins2.grid()
+    axins2.yaxis.set_label_position('right')
+    axins2.yaxis.labelpad = 20
+    axins2.set_ylabel('Y - pred', fontsize=13, rotation=270)
+    axins2.set_title('b)', loc='right', x=0.98, y=0.85)
+
+    plt.xticks(visible=True)
+    mark_inset(ax, axins, loc1=2, loc2=4, fc="none", ec="0.5")
+    plt.suptitle('Weekly Fitting at LSC - Hall A')
+    plt.show()
+    
 
 def show_errors(neurons, Xtrainlist, Y_train, Xtest_list, Y_test, arr_str, iterations, sample_size):
     for i in range(len(Xtrainlist)):
